04e075cbdad3111788c825534713d2679140a36b
Andres Miguel Airabella <tuk@satellogic.com>
2018-11-16 16:47:28 -0300

    codigos test python

---
+++ test_python/risc-v.py
@@ -0,0 +1,26 @@
+# Test for axi4 Lite Slave device
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge, FallingEdge, Edge, Event
+from cocotb.result import TestFailure, TestError, ReturnValue, SimFailure
+from cocotb.binary import BinaryValue
+
+CLK_PERIOD = 10
+
+@cocotb.coroutine
+def Reset(dut):
+    dut.RST_i <=  0
+    yield Timer(CLK_PERIOD * 10)
+    dut.RST_i  <= 1
+
+@cocotb.test()
+def test(dut):
+    """
+    Description:
+        Test RISC-V Simple Implementation
+    """
+    cocotb.fork(Clock(dut.CLK_i, CLK_PERIOD).start())
+    yield Reset(dut)
+
+    yield Timer(CLK_PERIOD * 300)
